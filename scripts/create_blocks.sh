3ba04dc5ddc1d81b665fe4b90b7306e6eec8d213
masato25 <leomasato25@gmail.com>
2021-04-28 17:03:55 +0800

    chore: add anchor peer config tx generator

--- scripts/create_blocks.sh
+++ scripts/create_blocks.sh
@@ -14,7 +14,7 @@ configtxgen -profile SampleMultiNodeEtcdRaft -outputBlock ./config/genesis.block
 configtxgen -profile Channel -outputCreateChannelTx ./config/${CHANNELID}.tx \
 -channelID ${CHANNELID}
 
-configtxgen -profile Channel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID mychannel -asOrg Org1MSP
-
+configtxgen -profile Channel -outputAnchorPeersUpdate ./config/channel-artifacts/${CHANNELID}_Org1MSPanchors.tx -channelID ${CHANNELID} -asOrg Org1MSP
+#configtxgen -profile Channel -outputAnchorPeersUpdate ./config/channel-artifacts/${CHANNELID}_Org2MSPanchors.tx -channelID ${CHANNELID} -asOrg Org2MSP
 
 docker-compose up -d o1.orderer.example.com o2.orderer.example.com o3.orderer.example.com

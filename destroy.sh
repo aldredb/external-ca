a710d2f3df10707b6d478f97ebd9beccef70a56e
masato25 <leomasato25@gmail.com>
2021-04-28 22:15:50 +0800

    chore: comment command to avoiding reset config

--- destroy.sh
+++ destroy.sh
@@ -7,7 +7,7 @@ docker-compose down
 docker rm -f $(docker ps -a | grep chaincode1 | awk '{print $1}')
 
 sudo rm -rf config/* crypto-config identity-rca tls-rca newica
-sudo rm -rf ca-config
-git checkout ca-config
+# sudo rm -rf ca-config
+# git checkout ca-config
 
 echo "Done!!"

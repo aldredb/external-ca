741b9794f88cf6f2e04956cd8dbc96ba1e04bce3
masato25 <leomasato25@gmail.com>
2021-04-29 10:56:22 +0800

    chore: support replace strings with markdown

--- scripts/rename_orderer_org.sh
+++ scripts/rename_orderer_org.sh
@@ -18,12 +18,12 @@ REPLACEORGMSPNAME=Orderer
 echo "Current os: $machine"
 if [ $machine = "Linux" ]
 then
-  find ./ ! -iname 'rename*.sh' -type f \( -iname \*.yaml -o -iname \*.sh -o -iname \*.cnf \) -exec sed -i "s/$CURRENTORGDOMAIN/$REPLACEORGDOMAIN/g" {} \;
-  find ./ ! -iname 'rename*.sh' -type f \( -iname \*.yaml -o -iname \*.sh -o -iname \*.cnf \) -exec sed -i "s/$CURRENTORGDOMAIN/$REPLACEORGMSPNAME/g" {} \;
+  find ./ ! -iname 'rename*.sh' -type f \( -iname \*.yaml -o -iname \*.sh -o -iname \*.cnf -o -iname \*.md \) -exec sed -i "s/$CURRENTORGDOMAIN/$REPLACEORGDOMAIN/g" {} \;
+  find ./ ! -iname 'rename*.sh' -type f \( -iname \*.yaml -o -iname \*.sh -o -iname \*.cnf -o -iname \*.md \) -exec sed -i "s/$CURRENTORGDOMAIN/$REPLACEORGMSPNAME/g" {} \;
 elif [ $machine = "Mac" ]
 then
-  find ./ ! -iname 'rename*.sh' -type f \( -iname \*.yaml -o -iname \*.sh -o -iname \*.cnf \) -exec sed -i '' -e "s/$CURRENTORGDOMAIN/$REPLACEORGDOMAIN/g" {} \;
-  find ./ ! -iname 'rename*.sh' -type f \( -iname \*.yaml -o -iname \*.sh -o -iname \*.cnf \) -exec sed -i '' -e "s/$CURRENTORGDOMAIN/$REPLACEORGMSPNAME/g" {} \;
+  find ./ ! -iname 'rename*.sh' -type f \( -iname \*.yaml -o -iname \*.sh -o -iname \*.cnf -o -iname \*.md \) -exec sed -i '' -e "s/$CURRENTORGDOMAIN/$REPLACEORGDOMAIN/g" {} \;
+  find ./ ! -iname 'rename*.sh' -type f \( -iname \*.yaml -o -iname \*.sh -o -iname \*.cnf -o -iname \*.md \) -exec sed -i '' -e "s/$CURRENTORGDOMAIN/$REPLACEORGMSPNAME/g" {} \;
 else
   echo "$machine is unsupported!"
 fi
